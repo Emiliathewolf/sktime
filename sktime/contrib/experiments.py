Fixed bug with overwriting scaling contestants

sktime/contrib/experiments.py
@@ -58,6 +58,8 @@ from sktime.utils.data_io import load_from_tsfile_to_dataframe as load_ts
 import sktime.datasets.tsc_dataset_names as dataset_lists
 from sktime.datasets.base import load_UCR_UEA_dataset
 
+from scipy.spatial.distance import pdist
+
 __author__ = ["Tony Bagnall"]
 
 """ Prototype mechanism for testing classifiers on the UCR format. This mirrors the
@@ -208,6 +210,40 @@ def stratified_resample(X_train, y_train, X_test, y_test, random_state):
     return X_train, y_train, X_test, y_test
 
 
+def us(query, p):
+    n = query.size
+    QP = np.empty(shape=(n, p))
+    # p / n = scaling factor
+    for i in range(n):
+        curQ = query.iloc[i][0]
+        for j in range(p):
+            QP[i][j] = (curQ[int(j * (n / p))])
+    return QP
+
+# Yes I know sklearn has this, no I don't care
+def euclidian_distances(q):
+    ED = sum(pdist(np.array(q), 'sqeuclidean'))
+    return ED
+
+def compare_scaling(query, min = 1, max = None):
+    best_match_value = float('inf')
+    best_match = None
+
+    if max == None:
+        max = 0
+        for i in range(query.size):
+            if query.iloc[i][0].size > max:
+                max = query.iloc[i][0].size
+    n = min
+    m = max
+    for p in range(n, m):
+        QP = us(query, p)
+        dist = euclidian_distances(QP)  # Compare like sizes
+        if dist < best_match_value:
+            best_match_value = dist
+            best_match = QP
+    return np.array(best_match)
+
 def run_experiment(
     problem_path,
     results_path,
@@ -284,6 +320,11 @@ def run_experiment(
     # currently only works with .ts
     trainX, trainY = load_ts(problem_path + dataset + "/" + dataset + "_TRAIN" + format)
     testX, testY = load_ts(problem_path + dataset + "/" + dataset + "_TEST" + format)
+
+    #Uniform scalining 'ere
+    #newX = compare_scaling(trainX)
+    #exit()
+
     if resampleID != 0:
         # allLabels = np.concatenate((trainY, testY), axis = None)
         # allData = pd.concat([trainX, testX])
@@ -668,11 +709,11 @@ benchmark_datasets = [
     "GestureMidAirD1",
     "GestureMidAirD2",
     "GestureMidAirD3",
-    "GesturePebbleZ1",
-    "GesturePebbleZ2",
-    "PickupGestureWiimoteZ",
-    "PLAID",
-    "ShakeGestureWiimoteZ",
+    #"GesturePebbleZ1",
+    #"GesturePebbleZ2",
+    #"PickupGestureWiimoteZ",
+    #"PLAID",
+    #"ShakeGestureWiimoteZ",
 ]
 
 if __name__ == "__main__":

sktime/series_as_features/base/estimators/interval_based/_tsf.py
@@ -212,23 +212,8 @@ class BaseTimeSeriesForest:
         output = np.sum(y_probas, axis=0) / (
             np.ones(self.n_classes) * self.n_estimators
         )
-        #output = np.sum(y_probas, axis=0) / (
-        #    np.ones(self.n_classes) * self.n_estimators
-        #)
         return output
 
-
-def us(query, scale_to, min = 0, max = None):
-    n = len(query)
-    m = scale_to
-
-    QP = []
-    # p / n = scaling factor
-    if max is None:
-        max = m
-    for j in range(min, max):
-        QP.append(query[int(j*(n/m))])
-    return QP
 
 def _transform(X, intervals, unequal):
     """Compute the mean, standard deviation and slope for given intervals
@@ -281,24 +266,26 @@ def _transform(X, intervals, unequal):
             # We want the scale length to be greater than the min length to ensure we're not always picking the end
             for s in range(min_length, int(min_length + 1)):
                 # Scale
-                scaled = np.empty(shape=(int(n_instances), int(intervals[j][1] - intervals[j][0])))
+                #scaled = np.empty(shape=(int(n_instances), intervals[j][1] - intervals[j][0]))
+                scaled = []
+                # too long, returning nans
                 curX = None
                 lenX = None
 
                 lower_interval = int(s * low_diff)
                 upper_interval = int(s / high_diff)
-                if upper_interval == lower_interval:
-                    upper_interval += 1
-                # Swap
-                #if lower_interval > upper_interval:
-                #    temp = upper_interval
-                #    upper_interval = lower_interval
-                #    lower_interval = temp
+
+                if lower_interval > upper_interval:
+                    temp = upper_interval
+                    upper_interval = lower_interval
+                    lower_interval = temp
 
                 for i in range(n_instances):
                     # Append the scaled and sliced result
                     curX = X.iloc[i][0]
                     lenX = len(curX)
+
+
 
                     # Prevent it going over
                     # I think this has stuff to do with the scaling based on longest, if there are intervals that take place
@@ -313,37 +300,46 @@ def _transform(X, intervals, unequal):
                             lower_interval = upper_interval
                             upper_interval = temp
                         diff = upper_interval
-                        upper_interval = int(((lenX * s) / lenX))
+                        upper_interval = int(((lenX * s) / lenX)) - 2
                         diff -= upper_interval
                         lower_interval -= diff
                     if lower_interval < 0:
                         lower_interval = 0
 
-                    for o in range(lower_interval, upper_interval):
-                        #Nans being written?
-                        scaled[i] = (curX[int(o * (lenX / s))])
+                    if (upper_interval == lower_interval or upper_interval == lower_interval + 1) and upper_interval + 2 * (lenX / s) < lenX:
+                        if upper_interval == lower_interval + 1:
+                            upper_interval += 1
+                        else:
+                            upper_interval += 2
+                    elif (upper_interval == lower_interval or upper_interval == lower_interval + 1) and upper_interval + 2 * (lenX / s) > lenX:
+                        lower_interval -= 2
 
-                #Best not being saved
-                ED = sum(pdist(np.array(scaled), 'sqeuclidean'))
-                if ED < best_match_value:
-                    best_match_value = ED
-                    best_X_scale = scaled
+
+
+                    scaled.append([])
+                    for o in range(lower_interval, upper_interval):
+                        scaled[i].append(curX[int(o * (lenX / s))])
+
+
+                scaled = np.array(scaled)
+                try:
+                    ED = sum(pdist(scaled, 'sqeuclidean'))
+                    if ED < best_match_value:
+                        best_match_value = ED
+                        best_X_scale = scaled
+                except:
+                    print("Bad ED")
+
 
             # For some reason best_X_scale isn't set but X_slice is?
             if len(best_X_scale) != 0:
                 X_slice = best_X_scale
-            X_slice = np.array(X_slice)
+            else:
+                X_slice = np.array(X_slice)
         else:
             X_slice = X[:, intervals[j][0] : intervals[j][1]]
 
-        try:
-            means = np.mean(X_slice, axis=1)
-        except:
-            print("test")
-            print("X Slice: ", X_slice)
-            print("Upper: ", upper_interval)
-            print("Lower: ", lower_interval)
-            print("Longest: ", longest_length)
+        means = np.mean(X_slice, axis=1)
         std_dev = np.std(X_slice, axis=1)
         slope = _slope(X_slice, axis=1)
         transformed_x[3 * j] = means